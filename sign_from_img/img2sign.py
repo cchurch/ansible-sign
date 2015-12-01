9e1df702182fd48987986e31e42f0b6d60a685cd
Chris Church <chris@ninemoreminutes.com>
2015-11-30 22:33:30 -0500

    Add example of loading sign pattern from an image.

---
+++ sign_from_img/img2sign.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# Python
+import sys
+
+# Pillow
+from PIL import Image
+
+# Jinja2
+import jinja2
+
+
+def get_img_data(filename):
+    img = Image.open(filename)
+    img = img.quantize(32).convert('RGBA')
+    colors = [x[1] for x in img.getcolors()]
+    data = list(img.getdata())
+    rows = []
+    for rowpixel in xrange(0, len(data), img.width):
+        rowdata = data[rowpixel:rowpixel + img.width]
+        rowmap = [colors.index(x) for x in rowdata]
+        row = []
+        for rowm in rowmap:
+            try:
+                if row[-1] == rowm:
+                    row[-2] += 1
+                    continue
+            except IndexError:
+                pass
+            row.extend([1, rowm])
+        rows.append(row)
+    color_map = [dict(zip('rgba', x)) for x in colors]
+    return {'color_map': color_map, 'rows': rows}
+
+
+def output_ino(img_data):
+    loader = jinja2.FileSystemLoader(searchpath='.')
+    env = jinja2.Environment(loader=loader)
+    template = env.get_template('sign_from_img.ino.j2')
+    return template.render(img_data)
+
+
+def main():
+    img_data = get_img_data(sys.argv[1]) or {}
+    print output_ino(img_data).encode('utf-8')
+
+
+if __name__ == '__main__':
+    main()
